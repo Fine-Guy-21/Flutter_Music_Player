ceca97ebe4da6b1681e119819293556ce751467e
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2025-07-27 18:30:48 +0300

    Update colors.dart

--- fine_music_player/lib/model/colors.dart
+++ fine_music_player/lib/model/colors.dart
@@ -5,8 +5,10 @@ class AppColors {
   AppColors(this.isDarkMode);
 
   Color get primaryColor => isDarkMode ? Colors.amber : Colors.purpleAccent;
-  Color get primaryTextColor => isDarkMode ? Colors.white : Colors.black;
-  Color get backgroundColor => isDarkMode ? Colors.black : Colors.white;
+  Color get primaryTextColor =>
+      isDarkMode ? const Color.fromARGB(200, 255, 255, 255) : Colors.black;
+  Color get backgroundColor =>
+      isDarkMode ? Colors.black : const Color.fromARGB(255, 255, 255, 255);
   Color get unchangableColor =>
       isDarkMode ? Colors.grey.shade400 : Colors.grey.shade800;
 }
